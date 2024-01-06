2b9b8527b5106160226b1af2e6c74ffb94b32d81
gabrielemrt <gabriele.martini.03@gmail.com>
2024-01-06 17:14:32 +0100

    --

--- servomotor/v5/static/script.js
+++ servomotor/v5/static/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const handle = document.getElementById('joystick-handle');
   const axisXElement = document.getElementById('axis-x');
   const axisYElement = document.getElementById('axis-y');
+  const savedCoordinatesElement = document.getElementById('saved-coordinates');
 
   const joystickRadius = joystick.clientWidth / 2;
   let isDragging = false;
@@ -14,47 +15,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function startDragging(e) {
     isDragging = true;
-    handle.style.transition = 'none';
+    handle.style.transition = 'transform 0.1s ease-out';
     handleDragging(e);
   }
 
   function stopDragging() {
     isDragging = false;
-    handle.style.transition = '0.3s ease-out';
     resetHandle();
   }
 
   function handleDragging(e) {
+    const rect = joystick.getBoundingClientRect();
+    const centerX = rect.left + joystickRadius;
+    const centerY = rect.top + joystickRadius;
+    let deltaX = e.clientX - centerX;
+    let deltaY = e.clientY - centerY;
+
+    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+    if (distance > joystickRadius) {
+      const angle = Math.atan2(deltaY, deltaX);
+      deltaX = joystickRadius * Math.cos(angle);
+      deltaY = joystickRadius * Math.sin(angle);
+    }
+
+    const posX = deltaX / joystickRadius;
+    const posY = deltaY / joystickRadius;
+
+    handle.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    updateAxisValues(posX, posY);
+
+    // Mappa l'intervallo -1 a 1 nel range 0 a 180
+    const mappedX = map(posX, -1, 1, 0, 180);
+    const mappedY = map(posY, -1, 1, 0, 180);
+
     if (isDragging) {
-      const rect = joystick.getBoundingClientRect();
-      const centerX = rect.left + joystickRadius;
-      const centerY = rect.top + joystickRadius;
-      let deltaX = e.clientX - centerX;
-      let deltaY = e.clientY - centerY;
-
-      const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
-      if (distance > joystickRadius) {
-        const angle = Math.atan2(deltaY, deltaX);
-        deltaX = joystickRadius * Math.cos(angle);
-        deltaY = joystickRadius * Math.sin(angle);
-      }
-
-      const posX = deltaX / joystickRadius;
-      const posY = deltaY / joystickRadius;
-
-      handle.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
-      updateAxisValues(posX, posY);
-
-      // Mappa l'intervallo -1 a 1 nel range 0 a 180
-      const mappedX = map(posX, -1, 1, 0, 180);
-      const mappedY = map(posY, -1, 1, 0, 180);
-
-      // Invia le coordinate al server Flask
+      // Invia le coordinate al server Flask solo se il joystick è in movimento
       sendCoordinates(mappedX, mappedY);
     }
   }
 
   function resetHandle() {
+    handle.style.transition = 'transform 0.3s ease-out';
     handle.style.transform = 'translate(0, 0)';
     updateAxisValues(lastPosition.x, lastPosition.y);
     sendCoordinates(lastPosition.x, lastPosition.y);
@@ -63,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function updateAxisValues(x, y) {
     axisXElement.textContent = `X: ${Math.round(x * 180)}`;
     axisYElement.textContent = `Y: ${Math.round(y * 180)}`;
+    savedCoordinatesElement.textContent = `Saved: ${Math.round(lastPosition.x * 180)}, ${Math.round(lastPosition.y * 180)}`;
   }
 
   function sendCoordinates(x, y) {
