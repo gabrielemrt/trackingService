0d2a4f1738b95bddc10f9df0546f698d0303aab5
gabrielemrt <gabriele.martini.03@gmail.com>
2024-01-10 12:47:04 +0100

    .

--- cameraStreaming/installation.sh
+++ cameraStreaming/installation.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-## INSTALLAZIONE DEL SERVIZIO DI STREAMING
-figlet "INSTALLATION SERVER STREAMING"
+### INSTALLAZIONE DEL SERVIZIO DI STREAMING
+#figlet "INSTALLATION SERVER STREAMING"
+#sudo apt-get update
+#sudo snap install mjpg-streamer
+
+
+## INSTALLAZIONE STRUMENTI SVILUPPATORE
+figlet "INSTALLAZIONE STRUMENTI SVILUPPATORE"
 sudo apt-get update
-sudo apt-get install mjpg-streamer -y
+sudo apt-get upgrade -y
+sudo apt-get install build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev -y
 
 
-## AVVIO IL SERVIZIO DI STREAMING
-figlet "START STREAMING SERVICE"
-mjpg_streamer -i "input_raspicam.so" -o "output_http.so -p 8080"
-
-
-
-## 1. Install system dependencies:
-sudo apt-get update && sudo apt-get install -y python3-dev libjpeg-dev libatlas-base-dev raspi-gpio libhdf5-dev python3-smbus
-
-## 2. Create a new project directory:
-mkdir rpi-deep-pantilt && cd rpi-deep-pantilt
-
-## 3. Create a new virtual environment:
-python3 -m venv .venv
-
-## 4. Activate the virtual environment:
-source .venv/bin/activate && python3 -m pip install --upgrade pip
-
-## 5. Install TensorFlow 2.0 from a community-built wheel:
-pip install https://github.com/leigh-johnson/Tensorflow-bin/blob/master/tensorflow-2.0.0-cp37-cp37m-linux_armv7l.whl?raw=true
-
-## 6. Install the rpi-deep-pantilt Python package:
-python3 -m pip install rpi-deep-pantilt
+## INSTALLAZIONE STRUMENTI SVILUPPATORE
+figlet "INSTALLAZIONE OPENCV"
+sudo pip3 install opencv-python
